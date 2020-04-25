f90ead3f2613f2018198709db4ce1cd623248d4b
Natalia Purtova <3716987@gmail.com>
2020-04-25 23:40:09 +0300

    only js, 2 pages to learn

--- js.js
+++ js.js
@@ -2,16 +2,17 @@
 function clickOnMe() {
 	alert('Работает кнопачка')
 }
-//function buyButton() {
-//	alert("+1");
-//	document.querySelector('.likeCounter').innerText = +1;
-//}
 
-let name;
-let age = prompt('How old are you?');
-if (age <= 18) {
-	alert('Go home')
-} else {
-	name = prompt('Как тебя зовут?');
-	alert('Welcome, ' + name);
+function buyButton() {
+	alert("+1");
+	document.querySelector('.likeCounter').innerText = +1;
 }
+//
+//let name;
+//let age = prompt('How old are you?');
+//if (age <= 18) {
+//	alert('Go home')
+//} else {
+//	name = prompt('Как тебя зовут?');
+//	alert('Welcome, ' + name);
+//}
